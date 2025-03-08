83010bc890bc3ad3d403e46c1551f9a9f808d2ef
Chlomaki <177792266+chlomaki1@users.noreply.github.com>
2025-03-08 14:53:40 -0500

    Create website directory

---
+++ apps/website/src/app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+	children,
+}: Readonly<{ children: React.ReactNode; }>) {
+	return (
+		<html lang="en">
+			<body>
+				<div id="app">
+					{children}
+				</div>
+			</body>
+		</html>
+	);
+}
