2d309d22de95555b52ad27705ae15a9998190bea
Chlomaki <177792266+chlomaki1@users.noreply.github.com>
2025-03-09 01:37:09 -0500

    Assign unix sockets properly

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, net, io, net::SocketAddr};
+use std::{env, net, io, net::SocketAddr, fs};
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use dotenvy::dotenv;
 use errors::LovedError;
@@ -10,57 +10,48 @@ pub mod state;
 pub mod errors;
 
 #[cfg(unix)]
-fn bind_address() -> io::Result<SocketAddr> {
-    let listener = tokio::net::UnixListener::bind("/tmp/loved_server.sock")?;
-
-    Ok(listener.local_addr()?)
-}
-
-#[cfg(not(unix))]
-fn bind_address() -> io::Result<SocketAddr> {
-    use std::net::{IpAddr, Ipv4Addr};
-
-    Ok(SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-        env::var("SERVER_PORT")
-            .expect("A port must be provided to run this application")
-            .parse::<u16>()
-            .unwrap(),
-    ))
-}
+const SOCKET_PATH: &str = "/tmp/loved_server.sock";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().expect("A proper environmental file has not been found");
 
-    // Initialize the state here as the new function is async
+    // Initialize the state
     let state = LovedState::new().await;
-    let workers = if env::var("APP_ENVIRONMENT").unwrap_or("development".to_string()) == "production".to_string() {
+    let workers = if env::var("APP_ENVIRONMENT").unwrap_or_else(|_| "development".to_string()) == "production" {
         8
     } else {
         4
     };
 
-    HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
             .wrap(Logger::new("%a %{User-Agent}i"))
-            .app_data(web::JsonConfig::default()
-                .error_handler(|err, _| LovedError::from(err).into())
-            )
+            .app_data(web::JsonConfig::default().error_handler(|err, _| LovedError::from(err).into()))
             .app_data(web::Data::new(state.clone()))
-
             // /oauth
             .service(
                 web::scope("/oauth")
                     .service(routes::oauth::start_token)
-                    .service(routes::oauth::login_token_callback)
+                    .service(routes::oauth::login_token_callback),
             )
-
-            // /*
             .default_service(web::route().to(routes::handle_default))
     })
-    .workers(workers)
-    .bind(bind_address()?)?
-    .run()
-    .await
+    .workers(workers);
+
+    #[cfg(unix)]
+    {
+        let _ = fs::remove_file(SOCKET_PATH); // Remove old socket if exists
+        server.bind_uds(SOCKET_PATH)?.run().await
+    }
+
+    #[cfg(not(unix))]
+    {
+        let port: u16 = env::var("SERVER_PORT")
+            .expect("A port must be provided")
+            .parse()
+            .unwrap();
+
+        server.bind(("127.0.0.1", port))?.run().await
+    }
 }
