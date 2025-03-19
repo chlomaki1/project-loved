ab0803a1f04635ae8b6b7dc31b4d816553689e46
Saryu <yuriko.onozuka@proton.me>
2025-03-19 11:59:03 -0400

    Add eslint config file

---
+++ apps/website/eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import eslint from "@eslint/js";
+import tsEslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default [
+    eslint.configs.recommended,
+    ...tsEslint.configs.recommended,
+    stylistic.configs["recommended-flat"],
+    {
+        plugins: {
+            "@stylistic": stylistic
+        },
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/comma-dangle": ["warn", "never"],
+            "@stylistic/quotes": ["warn", "double"],
+            "@stylistic/semi": ["warn", "always"]
+        }
+    }
+];
