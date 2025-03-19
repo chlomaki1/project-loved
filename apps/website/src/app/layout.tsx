31ba82c79d620dcea901dc6f03262af75d076eb0
Saryu <yuriko.onozuka@proton.me>
2025-03-19 11:59:38 -0400

    Apply eslint configuration

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,13 +1,13 @@
 export default function RootLayout({
-	children,
-}: Readonly<{ children: React.ReactNode; }>) {
-	return (
-		<html lang="en">
-			<body>
-				<div id="app">
-					{children}
-				</div>
-			</body>
-		</html>
-	);
+    children
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+            <body>
+                <div id="app">
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
 }
